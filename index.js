c7b5e6b894d2f2922e48f43097d475d9193c14b4
abr-ya <gamer05@list.ru>
2022-05-07 08:57:50 +0300

    feat: Add Books router and data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const helloRouter = require("./routes/hello");
+const booksRouter = require("./routes/books");
+const db = require("./data/db");
 
 const app = express();
 app.use(cors());
@@ -10,8 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// routers
+// routers and data
+app.db = db;
 app.use("/", helloRouter);
+app.use("/books", booksRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
